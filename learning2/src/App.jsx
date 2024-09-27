efbd67403c1d58b387a2b31b8e746b61b22d093e
Rahul <rs9462784650@gmail.com>
2024-09-27 12:55:11 +0530

    Bg_color changer

--- learning2/src/App.jsx
+++ learning2/src/App.jsx
@@ -1,11 +1,50 @@
-import "./App.css";
+import { useState } from "react";
+// import "./App.css";
 
 function App() {
+
+  const [color, setcolor] = useState ('green')
   return (
     <>
-      <h1 className="bg">Tailwind test</h1>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+        <button 
+        onClick={()=>setcolor('red')}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{background: 'red'}}
+         >Red</button>
+        <button 
+        onClick={()=>setcolor('Green')}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{background: 'Green'}}
+         >Green</button>
+        <button 
+        onClick={()=>setcolor('black')}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{background: 'black'}}
+         >black</button>
+        <button 
+        onClick={()=>setcolor('purple')}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{background: 'purple'}}
+         >purple</button>
+        <button 
+        onClick={()=>setcolor('orange')}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{background: 'orange'}}
+         >orange</button>
+
+        </div>
+
+        </div>
+      </div>
+      
     </>
   );
 }
 
 export default App;
+
